4f8f03a3c151f2d7a77d8cedd4f30fba876a6267
Richard Jones <itszootime@gmail.com>
2012-07-04 14:57:46 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emulatorization::Application.config.secret_token = '6ef39c99809a1b101d6b02bb4b96283ac6596c9eb588d638449d7f8e99c6a2f4a4d9eefc9ad50f68f23f37eaadc1e8d813d75a49287fa12ed70e7d85c2d6947f'
